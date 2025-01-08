5ea5fe6042e574cb2ab93d68b5813ee61720311d
Cestercian <yashafaid@gmail.com>
2025-01-08 23:49:05 +0530

    V.1.3

--- Main.jsx
+++ Main.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import Ingredients from "./Ingredients.jsx";
 
 export default function Main() {
     const [ingredients, setIngredients] = useState([]);
@@ -38,21 +39,10 @@ export default function Main() {
                 <button type="submit">Add ingredient</button>
             </form>
 
-            {ingredients.length > 0 && (
-                <section>
-                    <h2>Ingredients on hand:</h2>
-                    <ul className="ingredients-list" aria-live="polite">
-                        {ingredientsListItems}
-                    </ul>
-                    {ingredients.length > 3 && <div className="get-recipe-container">
-                        <div>
-                            <h3>Ready for a recipe?</h3>
-                            <p>Generate a recipe from your list of ingredients.</p>
-                        </div>
-                        <button>Get a recipe</button>
-                    </div>}
-                </section>
-            )}
+            {ingredients.length > 0 && <Ingredients
+                ingredientsListItems={ingredientsListItems}
+                ingredients={ingredients}
+            />}
         </main>
     );
 }
