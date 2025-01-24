1abe33661eeda2a60fcb08877e42d43df7533ebd
Cestercian <yashafaid@gmail.com>
2025-01-24 23:45:22 +0530

    V.2.0

--- Components/ClaudeRecipe.jsx
+++ Components/ClaudeRecipe.jsx
@@ -1,12 +1,22 @@
+import GradientText from './GradientText'
+
+
 
 import Markdown from "react-markdown";
 export default function ClaudeRecipe(props) {
     return (
+        <GradientText
+            colors={["#40ffaa", "#4079ff", "#40ffaa", "#4079ff", "#40ffaa"]}
+            animationSpeed={3}
+            showBorder={false}
+            className="custom-class"
+        >
             <section className="suggested-recipe-container">
                 <h2>Chef Claude Recommends:</h2>
                 <Markdown>
-                {props.recipe}
+                    {props.recipe}
                 </Markdown>
             </section>
+        </GradientText>
     )
 }
